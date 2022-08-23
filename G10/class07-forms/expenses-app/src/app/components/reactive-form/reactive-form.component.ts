f5dbf6d5dc6af58795392f4980e8a031587b1c15
borisovski-borche <borisovski.borce@gmail.com>
2022-08-23 18:38:29 +0200

    finished forms

--- G10/class07-forms/expenses-app/src/app/components/reactive-form/reactive-form.component.ts
+++ G10/class07-forms/expenses-app/src/app/components/reactive-form/reactive-form.component.ts
@@ -8,6 +8,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
   expenseForm: FormGroup;
+  paymentType: string[] = ['cash', 'card'];
+  blockedWords: string[] = [
+    'money',
+    'casino',
+    'betting',
+    'french words',
+    'drugs',
+  ];
+
+  maxCommentLength: number = 30;
 
   constructor() {}
 
@@ -17,16 +27,36 @@ export class ReactiveFormComponent implements OnInit {
 
   initForm() {
     this.expenseForm = new FormGroup({
-      title: new FormControl('', [Validators.required]),
-      amount: new FormControl(0, [
-        Validators.required,
-        Validators.min(1),
-        Validators.max(500),
-      ]),
+      basicData: new FormGroup({
+        title: new FormControl('', [
+          Validators.required,
+          this.blockedWordsValidator,
+        ]),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(500),
+        ]),
+        date: new FormControl('2022-08-23', [Validators.required]),
+      }),
+      priority: new FormControl('medium'),
+      comment: new FormControl('', [Validators.maxLength(30)]),
+      expenseType: new FormControl('card'),
     });
   }
 
+  blockedWordsValidator = (
+    control: FormControl
+  ): { [key: string]: boolean } | null => {
+    if (this.blockedWords.includes(control.value)) {
+      return { wordIsForbidden: true };
+    }
+
+    return null;
+  };
+
   onFormSubmit() {
     console.log(this.expenseForm);
+    console.log(this.expenseForm.value);
   }
 }
