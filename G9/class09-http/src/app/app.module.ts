d5db846fe67a7948e03fe9b86f2b4500e2500840
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2022-08-25 17:24:49 +0200

    Added http init code

---
+++ G9/class09-http/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReviewListComponent } from './components/review-list/review-list.component';
+import { ReviewDetailsComponent } from './components/review-details/review-details.component';
+import { AddReviewComponent } from './components/add-review/add-review.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReviewListComponent,
+    ReviewDetailsComponent,
+    AddReviewComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
